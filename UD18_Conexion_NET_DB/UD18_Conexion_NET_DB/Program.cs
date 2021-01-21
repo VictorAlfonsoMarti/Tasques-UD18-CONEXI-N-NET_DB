63e8e3e302f9c7de897054fdde454c9a87b9a043
Víctor Alfonso Martí <victor19am@gmail.com>
2021-01-21 01:45:24 +0100

    Add files via upload

---
+++ UD18_Conexion_NET_DB/UD18_Conexion_NET_DB/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace UD18_Conexion_NET_DB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicios.Ejercicio_1();
+            //Ejercicios.Ejercicio_2();
+            //Ejercicios.Ejercicio_3();
+            //Ejercicios.Ejercicio_4();
+            //Ejercicios.Ejercicio_5();
+            //Ejercicios.Ejercicio_6();
+            //Ejercicios.Ejercicio_7();
+            //Ejercicios.Ejercicio_8();
+            Ejercicios.Ejercicio_9();
+
+        }
+    }
+}
